bf1a51f35244e5d4c2cd7d22a75ca7f1981eb5d3
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-01 18:02:36 +0100

    refactor: day 01
     What I did :
     - Applied "Simplify if statement" from intellij IDE
     - removed this prefix which does not add readability value
     - extracted variable to make more business sense
     - used one line return
     - Personal preference : added lines before and inside method
       I find it easier to read the return statement as it is separated from the rest of the method body
    
     What I did not :
    
     - extract method for expiryDateNotPassed and wasInspected (premature in my opinion)
    
     🤔 Not sure what UUID inspectorId stands for.
     I did my own guess and decided that it was the inspector that approved the food.
     ⚠️ If it was real production code I would have asked the business about inspector entity.
    
    I hesitated between one line and column return style.
    I opted for one line style since there are only 3 variables.
    It looked for easy to read in my opinion.
    I would have opted for column style if there were more than 3 variables.
    
     Column style
     ```java
     return expiryDateNotPassed
           && approvedForConsumption
           && wasInspected;
    ```
    
    One line style
    ```java
     return expiryDateNotPassed && approvedForConsumption && wasInspected;
     ```

--- exercise/day01/src/main/java/food/Food.java
+++ exercise/day01/src/main/java/food/Food.java
@@ -7,13 +7,39 @@ import java.util.function.Supplier;
 public record Food(LocalDate expirationDate,
                    Boolean approvedForConsumption,
                    UUID inspectorId) {
+
     public boolean isEdible(Supplier<LocalDate> now) {
-        if (this.expirationDate.isAfter(now.get()) &&
-                this.approvedForConsumption &&
-                this.inspectorId != null) {
-            return true;
-        } else {
-            return false;
-        }
+
+        boolean expiryDateNotPassed = expirationDate.isAfter(now.get());
+        boolean wasInspected = inspectorId != null;
+
+        return expiryDateNotPassed && approvedForConsumption && wasInspected;
     }
 }
+
+// What I did :
+// - removed this prefix which does not add readability value
+// - extracted variable to make more business sense
+// - used one line return
+// - Personal preference : added lines before and inside method
+//   I find it easier to read the return statement as it is separated from the rest of the method body
+
+// What I did not :
+// - extract method for expiryDateNotPassed and wasInspected (premature in my opinion)
+
+// 🤔 Not sure what UUID inspectorId stands for.
+// I did my own guess and decided that it was the inspector that approved the food.
+// ⚠️ If it was real production code I would have asked the business about inspector entity.
+
+// I hesitated between one line and column return style.
+// I opted for one line style since there are only 3 variables.
+// It looked for easy to read in my opinion.
+// I would have opted for column style if there were more than 3 variables.
+
+// Column style
+// return expiryDateNotPassed
+//       && approvedForConsumption
+//       && wasInspected;
+
+// One line style
+// return expiryDateNotPassed && approvedForConsumption && wasInspected;
