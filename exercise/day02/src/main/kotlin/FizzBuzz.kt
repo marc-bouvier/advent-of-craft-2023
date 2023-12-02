caffa90051ae5ef44be88fb2bbd8dfff803dcef1
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-02 15:59:15 +0100

    refactor: invert if and simplify condition

--- exercise/day02/src/main/kotlin/FizzBuzz.kt
+++ exercise/day02/src/main/kotlin/FizzBuzz.kt
@@ -1,21 +1,24 @@
 @file:JvmName("FizzBuzz")
+
 package games
- fun convert(input: Int): String {
-    return if (input > 0) {
-        if (input <= 100) {
-            if (input % 3 == 0 && input % 5 == 0) {
-                return "FizzBuzz"
-            }
-            if (input % 3 == 0) {
-                return "Fizz"
-            }
-            if (input % 5 == 0) {
-                "Buzz"
-            } else input.toString()
-        } else {
-            throw OutOfRangeException()
+
+fun convert(input: Int): String {
+    if (input <= 0) {
+        throw OutOfRangeException()
+    }
+
+    if (input <= 100) {
+        if (input % 3 == 0 && input % 5 == 0) {
+            return "FizzBuzz"
         }
+        if (input % 3 == 0) {
+            return "Fizz"
+        }
+        return if (input % 5 == 0) {
+            "Buzz"
+        } else input.toString()
     } else {
         throw OutOfRangeException()
     }
+
 }
