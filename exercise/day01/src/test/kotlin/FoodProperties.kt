1c9451632f4742c8d28e9dd313aed26cc665e246
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-02 15:24:30 +0100

    test: property based testing in kotlin with kotest

--- exercise/day01/src/test/kotlin/FoodProperties.kt
+++ exercise/day01/src/test/kotlin/FoodProperties.kt
@@ -1,4 +1,5 @@
 import food.Food
+import food.FoodKt
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.localDate
@@ -8,10 +9,11 @@ import kotlinx.datetime.LocalDate
 import kotlinx.datetime.minus
 import java.util.*
 import kotlinx.datetime.toJavaLocalDate as toJava
+import kotlinx.datetime.toKotlinLocalDate as toKotlin
 
 class FoodProperties : StringSpec({
 
-    "is edible 1 day before expiration date" {
+    "Food is edible 1 day before expiration date" {
         val expirationDate = LocalDate(2023, 12, 24)
         forAll(Arb.localDate(maxDate = expirationDate.minus(1, DAY).toJava())) { now ->
             Food(
@@ -22,7 +24,29 @@ class FoodProperties : StringSpec({
         }
     }
 
-    "is not edible from expiration date" {
+    "Food is not edible from expiration date" {
+        val expirationDate = LocalDate(2023, 12, 24)
+        forAll(Arb.localDate(minDate = expirationDate.toJava())) { now ->
+            Food(
+                expirationDate.toJava(),
+                true,
+                UUID.randomUUID()
+            ).isEdible { now }.not()
+        }
+    }
+
+    "FoodKt is edible 1 day before expiration date" {
+        val expirationDate = LocalDate(2023, 12, 24)
+        forAll(Arb.localDate(maxDate = expirationDate.minus(1, DAY).toJava())) { now ->
+            FoodKt(
+                expirationDate,
+                true,
+                UUID.randomUUID()
+            ).isEdible { now.toKotlin() }
+        }
+    }
+
+    "FoodKt is not edible from expiration date" {
         val expirationDate = LocalDate(2023, 12, 24)
         forAll(Arb.localDate(minDate = expirationDate.toJava())) { now ->
             Food(
