2f249108da4edf44b77d33d6e0d484e35decd5f5
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-02 17:08:52 +0100

    refactor: remove suffix from exception : since it is thrown we know it is an exception.

--- exercise/day02/src/main/kotlin/FizzBuzz.kt
+++ exercise/day02/src/main/kotlin/FizzBuzz.kt
@@ -3,8 +3,8 @@
 package games
 
 fun convert(input: Int): String = when {
-    input <= 0 -> throw OutOfRangeException()
-    input > 100 -> throw OutOfRangeException()
+    input <= 0 -> throw OutOfRange()
+    input > 100 -> throw OutOfRange()
     input % 3 == 0 && input % 5 == 0 -> "FizzBuzz"
     input % 3 == 0 -> "Fizz"
     input % 5 == 0 -> "Buzz"
