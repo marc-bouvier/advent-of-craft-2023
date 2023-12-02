4dffe0ed4075cb798a1ff974026cd785e4ac58b5
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-02 15:57:07 +0100

    refactor: initial conversion to kotlin

--- exercise/day02/src/main/kotlin/FizzBuzz.kt
+++ exercise/day02/src/main/kotlin/FizzBuzz.kt
@@ -1,27 +1,21 @@
-package games;
-
-public class FizzBuzz {
-    private FizzBuzz() {
-    }
-    
-    public static String convert(Integer input) throws OutOfRangeException {
-        if (input > 0) {
-            if (input <= 100) {
-                if (input % 3 == 0 && input % 5 == 0) {
-                    return "FizzBuzz";
-                }
-                if (input % 3 == 0) {
-                    return "Fizz";
-                }
-                if (input % 5 == 0) {
-                    return "Buzz";
-                }
-                return input.toString();
-            } else {
-                throw new OutOfRangeException();
+@file:JvmName("FizzBuzz")
+package games
+ fun convert(input: Int): String {
+    return if (input > 0) {
+        if (input <= 100) {
+            if (input % 3 == 0 && input % 5 == 0) {
+                return "FizzBuzz"
             }
+            if (input % 3 == 0) {
+                return "Fizz"
+            }
+            if (input % 5 == 0) {
+                "Buzz"
+            } else input.toString()
         } else {
-            throw new OutOfRangeException();
+            throw OutOfRangeException()
         }
+    } else {
+        throw OutOfRangeException()
     }
 }
