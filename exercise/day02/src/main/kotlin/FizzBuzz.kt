2cfcf1dfd578638fc9ca10b33b74d209e5e50428
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-02 16:05:06 +0100

    refactor: lift return out of when expression

--- exercise/day02/src/main/kotlin/FizzBuzz.kt
+++ exercise/day02/src/main/kotlin/FizzBuzz.kt
@@ -3,14 +3,13 @@
 package games
 
 fun convert(input: Int): String {
-    when {
+    return when {
         input <= 0 -> throw OutOfRangeException()
         input > 100 -> throw OutOfRangeException()
-        input % 3 == 0 && input % 5 == 0 -> return "FizzBuzz"
-        input % 3 == 0 -> return "Fizz"
-        input % 5 == 0-> return "Buzz"
-        else -> return input.toString()
+        input % 3 == 0 && input % 5 == 0 -> "FizzBuzz"
+        input % 3 == 0 -> "Fizz"
+        input % 5 == 0 -> "Buzz"
+        else -> input.toString()
     }
-
-
 }
+
