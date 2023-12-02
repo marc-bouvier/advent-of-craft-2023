cd37eee361d9aed30ded1b29dcb64d6c6ce26cf1
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-02 16:02:58 +0100

    refactor: remove braces for all when entries

--- exercise/day02/src/main/kotlin/FizzBuzz.kt
+++ exercise/day02/src/main/kotlin/FizzBuzz.kt
@@ -4,21 +4,11 @@ package games
 
 fun convert(input: Int): String {
     when {
-        input <= 0 -> {
-            throw OutOfRangeException()
-        }
-        input > 100 -> {
-            throw OutOfRangeException()
-        }
-        input % 3 == 0 && input % 5 == 0 -> {
-            return "FizzBuzz"
-        }
-        input % 3 == 0 -> {
-            return "Fizz"
-        }
-        input % 5 == 0-> {
-            return "Buzz"
-        }
+        input <= 0 -> throw OutOfRangeException()
+        input > 100 -> throw OutOfRangeException()
+        input % 3 == 0 && input % 5 == 0 -> return "FizzBuzz"
+        input % 3 == 0 -> return "Fizz"
+        input % 5 == 0-> return "Buzz"
         else -> return input.toString()
     }
 
