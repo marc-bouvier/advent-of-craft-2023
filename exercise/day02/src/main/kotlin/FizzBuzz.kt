98a70bad6bcd6bd5b65823c1c8b3c7857d81683d
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-02 16:56:45 +0100

    refactor: replace toString with string template

--- exercise/day02/src/main/kotlin/FizzBuzz.kt
+++ exercise/day02/src/main/kotlin/FizzBuzz.kt
@@ -8,6 +8,6 @@ fun convert(input: Int): String = when {
     input % 3 == 0 && input % 5 == 0 -> "FizzBuzz"
     input % 3 == 0 -> "Fizz"
     input % 5 == 0 -> "Buzz"
-    else -> input.toString()
+    else -> "$input"
 }
 
