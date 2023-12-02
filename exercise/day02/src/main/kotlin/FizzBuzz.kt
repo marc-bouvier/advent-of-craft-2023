8bde47a9fcd75d742017ec06042e8fb8e9072313
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-02 16:05:21 +0100

    refactor: convert to expression body

--- exercise/day02/src/main/kotlin/FizzBuzz.kt
+++ exercise/day02/src/main/kotlin/FizzBuzz.kt
@@ -2,14 +2,12 @@
 
 package games
 
-fun convert(input: Int): String {
-    return when {
-        input <= 0 -> throw OutOfRangeException()
-        input > 100 -> throw OutOfRangeException()
-        input % 3 == 0 && input % 5 == 0 -> "FizzBuzz"
-        input % 3 == 0 -> "Fizz"
-        input % 5 == 0 -> "Buzz"
-        else -> input.toString()
-    }
+fun convert(input: Int): String = when {
+    input <= 0 -> throw OutOfRangeException()
+    input > 100 -> throw OutOfRangeException()
+    input % 3 == 0 && input % 5 == 0 -> "FizzBuzz"
+    input % 3 == 0 -> "Fizz"
+    input % 5 == 0 -> "Buzz"
+    else -> input.toString()
 }
 
