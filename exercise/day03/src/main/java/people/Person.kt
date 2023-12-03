099db83b520b2cf1e9dd1d8ab80dbd8c762a37cd
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-03 18:11:14 +0100

    refactor: convert to kotlin

--- exercise/day03/src/main/java/people/Person.kt
+++ exercise/day03/src/main/java/people/Person.kt
@@ -4,7 +4,7 @@ package people
 data class Person(@JvmField val firstName: String?, val lastName: String?, @JvmField val pets: MutableList<Pet?>) {
     constructor(firstName: String?, lastName: String?) : this(firstName, lastName, ArrayList<Pet?>())
 
-    fun addPet(petType: PetType?, name: String?, age: Int): Person {
+    fun addPet(petType: PetType, name: String, age: Int): Person {
         pets.add(Pet(petType, name, age))
         return this
     }
