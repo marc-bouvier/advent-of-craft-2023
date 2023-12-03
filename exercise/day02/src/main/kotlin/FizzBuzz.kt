e5276f41b3bcbca5dafc6ac620388d6a9c16e77b
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-03 20:29:11 +0100

    refactor(day02): merge edge cases exception

--- exercise/day02/src/main/kotlin/FizzBuzz.kt
+++ exercise/day02/src/main/kotlin/FizzBuzz.kt
@@ -3,8 +3,7 @@
 package games
 
 fun convert(input: Int): String = when {
-    input <= 0 -> throw OutOfRange()
-    input > 100 -> throw OutOfRange()
+    input <= 0 || input > 100 -> throw OutOfRange()
     input % 3 == 0 && input % 5 == 0 -> "FizzBuzz"
     input % 3 == 0 -> "Fizz"
     input % 5 == 0 -> "Buzz"
