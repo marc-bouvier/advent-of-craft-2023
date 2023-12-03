cbf50ba8f2125e82b159cabae26bcc5dda441ce7
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-03 18:22:27 +0100

    refactor: record attributs as java fields

--- exercise/day03/src/main/kotlin/people/Person.kt
+++ exercise/day03/src/main/kotlin/people/Person.kt
@@ -1,8 +1,14 @@
 package people
 
 @JvmRecord
-data class Person(@JvmField val firstName: String?, val lastName: String?, @JvmField val pets: MutableList<Pet?>) {
-    constructor(firstName: String?, lastName: String?) : this(firstName, lastName, ArrayList<Pet?>())
+data class Person(
+    @JvmField val firstName: String,
+    @JvmField val lastName: String,
+    @JvmField val pets: MutableList<Pet>
+) {
+
+    constructor(firstName: String, lastName: String) :
+            this(firstName, lastName, ArrayList<Pet>())
 
     fun addPet(petType: PetType, name: String, age: Int): Person {
         pets.add(Pet(petType, name, age))
