0c70e0c4f7e9d9a490e2e7610b86aaefe810e5e7
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-04 22:34:33 +0100

    refactor: explicit parameters

--- exercise/day04/src/test/kotlin/blog/ArticleTests.kt
+++ exercise/day04/src/test/kotlin/blog/ArticleTests.kt
@@ -20,7 +20,9 @@ class ArticleTests : StringSpec({
     // - adding an article
     // - text of the content
     "It should add a comment with the given text" {
+
         val article = anArticle()
+
         article.addComment(text = "Amazing article !!!", author = "Pablo Escobar")
 
         article.getComments() shouldHaveSingleElement { it.text == "Amazing article !!!" }
@@ -30,28 +32,34 @@ class ArticleTests : StringSpec({
     // - adding an article (already asserted)
     // - author of the content
     "It should add a comment with the given author" {
-        val article = anArticle()
-        val author = "Pablo Escobar"
-        article.addComment("Amazing article !!!", author)
 
-        article.getComments() shouldHaveSingleElement { it.author == author }
+        val article = anArticle()
+
+        article.addComment(text = "Amazing article !!!", author = "Pablo Escobar")
+
+        article.getComments() shouldHaveSingleElement { it.author == "Pablo Escobar" }
 
     }
 
     // No assertion, no date is visible in this test
     "It should add a comment with the date of the day" {
+
         val article = anArticle()
+
         shouldNotThrowAny {
-            article.addComment("Amazing article !!!", "Pablo Escobar")
+            article.addComment(text = "Amazing article !!!", author = "Pablo Escobar")
         }
     }
 
     // The behavior should be understandable by the business folks
     "It should throw an exception when adding existing comment" {
+
         val article = anArticle()
-        article.addComment("Amazing article !!!", "Pablo Escobar")
+
+        article.addComment(text = "Amazing article !!!", author = "Pablo Escobar")
+
         shouldThrow<CommentAlreadyExist> {
-            article.addComment("Amazing article !!!", "Pablo Escobar")
+            article.addComment(text = "Amazing article !!!", author = "Pablo Escobar")
         }
     }
 })
