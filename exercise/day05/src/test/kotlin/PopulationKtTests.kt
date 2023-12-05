718b4f594e5ba68b2276e629a7a03d6c429f04d0
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-05 12:48:04 +0100

    refactor(day05): Convert test to kotlin and kotest
    - typealias for collection
    - extension function for formatting

---
+++ exercise/day05/src/test/kotlin/PopulationKtTests.kt
@@ -0,0 +1,80 @@
+import io.kotest.core.spec.style.StringSpec
+import org.assertj.core.api.Assertions
+import people.Person
+import people.Pet
+import people.PetType
+
+typealias Population = List<Person>
+
+class PopulationKtTests : StringSpec({
+    lateinit var population: Population
+    beforeSpec {
+        population = listOf(
+            Person("Peter", "Griffin")
+                .addPet(PetType.CAT, "Tabby", 2),
+            Person("Stewie", "Griffin")
+                .addPet(PetType.CAT, "Dolly", 3)
+                .addPet(PetType.DOG, "Brian", 9),
+            Person("Joe", "Swanson")
+                .addPet(PetType.DOG, "Spike", 4),
+            Person("Lois", "Griffin")
+                .addPet(PetType.SNAKE, "Serpy", 1),
+            Person("Meg", "Griffin")
+                .addPet(PetType.BIRD, "Tweety", 1),
+            Person("Chris", "Griffin")
+                .addPet(PetType.TURTLE, "Speedy", 4),
+            Person("Cleveland", "Brown")
+                .addPet(PetType.HAMSTER, "Fuzzy", 1)
+                .addPet(PetType.HAMSTER, "Wuzzy", 2),
+            Person("Glenn", "Quagmire")
+        )
+    }
+
+    "Who owns the youngest pet" {
+        val filtered = population.stream()
+            .min(Comparator.comparingInt { person: Person -> youngestPetAgeOfThePerson(person) })
+            .orElse(null)!!
+        Assertions.assertThat(filtered.firstName).isEqualTo("Lois")
+    }
+
+    "People with their pets" {
+        val response = population.format()
+        Assertions.assertThat(response.toString())
+            .hasToString(
+                "Peter Griffin who owns : Tabby " + System.lineSeparator() +
+                        "Stewie Griffin who owns : Dolly Brian " + System.lineSeparator() +
+                        "Joe Swanson who owns : Spike " + System.lineSeparator() +
+                        "Lois Griffin who owns : Serpy " + System.lineSeparator() +
+                        "Meg Griffin who owns : Tweety " + System.lineSeparator() +
+                        "Chris Griffin who owns : Speedy " + System.lineSeparator() +
+                        "Cleveland Brown who owns : Fuzzy Wuzzy " + System.lineSeparator() +
+                        "Glenn Quagmire"
+            )
+    }
+
+})
+
+fun Population.format(): StringBuilder {
+    val response = StringBuilder()
+    for (person in this) {
+        response.append(String.format("%s %s", person.firstName, person.lastName))
+        if (!person.pets.isEmpty()) {
+            response.append(" who owns : ")
+        }
+        for (pet in person.pets) {
+            response.append(pet.name).append(" ")
+        }
+        if (this.last != person) {
+            response.append(System.lineSeparator())
+        }
+    }
+    return response
+}
+
+private fun youngestPetAgeOfThePerson(person: Person): Int {
+    return person.pets
+        .stream()
+        .mapToInt(Pet::age)
+        .min()
+        .orElse(Int.MAX_VALUE)
+}
