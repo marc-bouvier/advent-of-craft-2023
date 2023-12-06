0b8d02dce269a7e0c191155837e9d2a3cbdf96ff
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-06 18:15:27 +0100

    refactor(day06): remove "Returns" and "Throws an exception"

--- exercise/day06/src/test/kotlin/games/FizzBuzzTests.kt
+++ exercise/day06/src/test/kotlin/games/FizzBuzzTests.kt
@@ -5,77 +5,77 @@ import org.assertj.core.api.Assertions
 
 class FizzBuzzTests : StringSpec({
 
-    "Returns the given number for 1" {
+    "The given number for 1" {
         Assertions.assertThat(FizzBuzz.convert(1))
             .isEqualTo("1")
     }
 
-    "Returns the given number for 67" {
+    "The given number for 67" {
         Assertions.assertThat(FizzBuzz.convert(67))
             .isEqualTo("67")
     }
 
-    "Returns the given number for 82" {
+    "The given number for 82" {
         Assertions.assertThat(FizzBuzz.convert(82))
             .isEqualTo("82")
     }
 
-    "Returns fizz for 3" {
+    "Fizz for 3" {
         Assertions.assertThat(FizzBuzz.convert(3))
             .isEqualTo("Fizz")
     }
 
-    "Returns fizz for 66" {
+    "Fizz for 66" {
         Assertions.assertThat(FizzBuzz.convert(66))
             .isEqualTo("Fizz")
     }
 
-    "Returns fizz for 99" {
+    "Fizz for 99" {
         Assertions.assertThat(FizzBuzz.convert(99))
             .isEqualTo("Fizz")
     }
 
-    "Returns buzz for 5" {
+    "Buzz for 5" {
         Assertions.assertThat(FizzBuzz.convert(5))
             .isEqualTo("Buzz")
     }
 
-    "Returns buzz for 50" {
+    "Buzz for 50" {
         Assertions.assertThat(FizzBuzz.convert(50))
             .isEqualTo("Buzz")
     }
 
-    "Returns buzz for 85" {
+    "Buzz for 85" {
         Assertions.assertThat(FizzBuzz.convert(85))
             .isEqualTo("Buzz")
     }
 
-    "Returns fizz buzz for 15" {
+    "Fizz Buzz for 15" {
         Assertions.assertThat(FizzBuzz.convert(15))
             .isEqualTo("FizzBuzz")
     }
 
-    "Returns fizz buzz for 30" {
+    "Fizz Buzz for 30" {
         Assertions.assertThat(FizzBuzz.convert(30))
             .isEqualTo("FizzBuzz")
     }
 
-    "Returns fizz buzz for 45" {
+    "Fizz Buzz for 45" {
         Assertions.assertThat(FizzBuzz.convert(45))
             .isEqualTo("FizzBuzz")
     }
 
-    "Throws an exception for 0" {
+    "Out of range for 0" {
         Assertions.assertThatThrownBy { FizzBuzz.convert(0) }
             .isInstanceOf(OutOfRangeException::class.java)
     }
 
-    "Throws an exception for 101" {
+    "Out of range for 101" {
         Assertions.assertThatThrownBy { FizzBuzz.convert(101) }
             .isInstanceOf(OutOfRangeException::class.java)
     }
 
-    "Throws an exception for minus 1" {
+    "Out of range for minus 1" {
         Assertions.assertThatThrownBy { FizzBuzz.convert(-1) }
             .isInstanceOf(OutOfRangeException::class.java)
     }
