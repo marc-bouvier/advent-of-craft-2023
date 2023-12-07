2c1baf2da011b91454105e6917c1ca6237299778
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-07 13:17:11 +0100

    refactor(day07): automatically convert tests to kotlin

--- exercise/day07/src/test/kotlin/ci/CapturingLogger.kt
+++ exercise/day07/src/test/kotlin/ci/CapturingLogger.kt
@@ -1,24 +1,17 @@
-package ci;
+package ci
 
-import ci.dependencies.Logger;
+import ci.dependencies.Logger
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class CapturingLogger implements Logger {
-    private final List<String> lines = new ArrayList<>();
-
-    @Override
-    public void info(String message) {
-        lines.add("INFO: " + message);
+class CapturingLogger : Logger {
+    private val lines: MutableList<String> = ArrayList()
+    override fun info(message: String) {
+        lines.add("INFO: $message")
     }
 
-    @Override
-    public void error(String message) {
-        lines.add("ERROR: " + message);
+    override fun error(message: String) {
+        lines.add("ERROR: $message")
     }
 
-    public List<String> getLoggedLines() {
-        return lines;
-    }
+    val loggedLines: List<String>
+        get() = lines
 }
