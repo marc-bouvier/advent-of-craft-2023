57d3cf8831f9e61bf8b2e6dfad063076429150e6
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-10 17:42:53 +0100

    test(day07): migrate mock verify not called
    //  verify(emailer, never()).send(any())
    //  https://www.baeldung.com/kotlin/mockk-check-method-invoked
    // Different options
    //        verify(inverse = true) { emailer.send(any()) }
    //        verify(exactly = 0) { emailer.send(any()) }
    //        verify { emailer wasNot Called }

--- exercise/day07/src/test/kotlin/ci/PipelineKoTest.kt
+++ exercise/day07/src/test/kotlin/ci/PipelineKoTest.kt
@@ -27,19 +27,21 @@ import org.junit.jupiter.api.Assertions.assertEquals
 // "$1" {
 class PipelineKoTest : StringSpec({
 
+    // Can you see my face and ear me?
+    // i just see your code. trying to join call
     lateinit var log: CapturingLogger
-
-    val config = mockk<Config>()
-    val emailer = mockk<Emailer>()
+    lateinit var config: Config
+    lateinit var emailer: Emailer
     lateinit var pipeline: Pipeline
 
     beforeEach {
         log = CapturingLogger()
+        config = mockk<Config>()
+        emailer = mockk<Emailer>()
         pipeline = Pipeline(config, emailer, log)
     }
 
-    "test_mockk_framework"() {
-
+    "test_mockk_framework" {
         every { config.sendEmailSummary() } returns true
         justRun { emailer.send("Deployment completed successfully") }
         val project = Project.builder()
@@ -60,10 +62,11 @@ class PipelineKoTest : StringSpec({
     }
 
 
-    
-     "project_with_tests_that_deploys_successfully_with_email_notification"() {
 
-        every{ config.sendEmailSummary() } returns true
+    "project_with_tests_that_deploys_successfully_with_email_notification" {
+
+        every { config.sendEmailSummary() } returns true
+        justRun { emailer.send(any()) }
         val project = Project.builder()
             .setTestStatus(PASSING_TESTS)
             .setDeploysSuccessfully(true)
@@ -78,29 +81,38 @@ class PipelineKoTest : StringSpec({
                 "INFO: Sending email"
             ), log.loggedLines
         )
-        verify{ emailer.send("Deployment completed successfully") }
+        verify { emailer.send("Deployment completed successfully") }
     }
 
 
-//    "project_with_tests_that_deploys_successfully_without_email_notification" {
-//
-//        every{ config.sendEmailSummary() } returns false
-//        val project = Project.builder()
-//            .setTestStatus(PASSING_TESTS)
-//            .setDeploysSuccessfully(true)
-//            .build()
-//
-//        pipeline.run(project)
-//
-//        assertEquals(
-//            mutableListOf(
-//                "INFO: Tests passed",
-//                "INFO: Deployment successful",
-//                "INFO: Email disabled"
-//            ), log.loggedLines
-//        )
-//        verify{ emailer, never()).send(any() }
-//    }
+    "project_with_tests_that_deploys_successfully_without_email_notification" {
+
+        every { config.sendEmailSummary() } returns false
+        justRun { emailer.send(any()) }
+        val project = Project.builder()
+            .setTestStatus(PASSING_TESTS)
+            .setDeploysSuccessfully(true)
+            .build()
+
+        pipeline.run(project)
+
+        assertEquals(
+            mutableListOf(
+                "INFO: Tests passed",
+                "INFO: Deployment successful",
+                "INFO: Email disabled"
+            ), log.loggedLines
+        )
+// ne devrait pas être appelé
+//  verify(emailer, never()).send(any())
+//  https://www.baeldung.com/kotlin/mockk-check-method-invoked
+// Different options
+//        verify(inverse = true) { emailer.send(any()) }
+//        verify(exactly = 0) { emailer.send(any()) }
+//        verify { emailer wasNot Called }
+
+        verify(exactly = 0) { emailer.send(any()) }
+    }
 //
 //    
 //    "project_without_tests_that_deploys_successfully_with_email_notification" {
