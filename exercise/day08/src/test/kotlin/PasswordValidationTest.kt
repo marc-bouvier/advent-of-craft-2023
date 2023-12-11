2a62f3c58903c9d987f65a616520ea55a75655b8
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-12-11 12:37:48 +0100

    working

--- exercise/day08/src/test/kotlin/PasswordValidationTest.kt
+++ exercise/day08/src/test/kotlin/PasswordValidationTest.kt
@@ -1,30 +1,25 @@
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-//  mvn test && git commit -am working || git reset --hard
-// - Contains at least 8 characters
-// - Contains at least one capital letter
-// - Contains at least one lowercase letter
-// - Contains at least a number
-// - Contains at least a special character in this list `. * # @ $ % &`.
-// - Any other characters are not authorized.
-// The TDD rules are quite simple:
-//
-//    - Write production code only to pass a failing unit test.
-//    - Write no more of a unit test than sufficient to fail (compilation failures are failures).
-//    - Write no more production code than necessary to pass the one failing unit test.
-//
-//A gentle reminder, the steps to use the TDD magic can be applied this way:
-//
-
 // alias tcr="mvn test && git commit -am working || git reset --hard"
 // tcr
 class PasswordValidationTest : StringSpec({
 //    🔴 Write a failing test
 //    🟢 Make the test pass
 //    🔵 Refactor your code
+//    - Write production code only to pass a failing unit test.
+//    - Write no more of a unit test than sufficient to fail (compilation failures are failures).
+//    - Write no more production code than necessary to pass the one failing unit test.
 
-    "test"{
-    "a" shouldBe "a"
-}
+    "test" {
+        "a" shouldBe "a"
+    }
+
+    // - Contains at least 8 characters
+    // - Contains at least one capital letter
+    // - Contains at least one lowercase letter
+    // - Contains at least a number
+    // - Contains at least a special character in this list `. * # @ $ % &`.
+    // - Any other characters are not authorized.
+
 })
